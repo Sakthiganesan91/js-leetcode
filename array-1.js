28d4092e29d324026c2a8197c0dcc7ad4994efe8
SakthiganesanG_lumel <SakthiganesanG@lumel.com>
2025-09-23 11:39:28 +0530

    prob(1):program to concatenation of array

---
+++ array-1.js
@@ -0,0 +1,10 @@
+//1.JavaScript program to concatenation of array
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function (nums) {
+  return nums.concat(nums);
+  //return [...nums,...nums]
+};
